a9a39e1ab8ab080deaf0db5603005d692ccdd453
Sanjay Siva <realsanjaysiva@gmail.com>
2026-04-22 23:06:34 +0530

    move query logic query logic should be in crud

--- app/main.py
+++ app/main.py
@@ -1,13 +1,13 @@
 from fastapi import FastAPI, Depends, HTTPException
 from sqlalchemy.orm import Session
-from . import models, schemas, crud
+from . import schemas, crud
 from .database import engine, get_db
 
 app = FastAPI(title="Campus Payment System API")
 
-@app.get("/students/{student_id}")
+@app.get("/students/{student_id}", response_model=schemas.StudentResponse)
 def read_student(student_id: str, db: Session = Depends(get_db)):
-    db_student = db.query(models.Student).filter(models.Student.student_id == student_id).first()
+    db_student = crud.get_student(db, student_id=student_id)
     if db_student is None:
         raise HTTPException(status_code=404, detail="Student not found")
     return db_student
