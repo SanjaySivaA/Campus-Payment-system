1851495d7c18634eb42fc67b053ddb1959c87d1c
Sanjay Siva <realsanjaysiva@gmail.com>
2026-05-01 00:39:29 +0530

    add sign up queries

--- app/crud.py
+++ app/crud.py
@@ -38,6 +38,47 @@
 #     result = execute_as_role(db, "vendor_role", query, {"vendor_id": vendor_id})
 #     return result.mappings().first()
 
+from . import schemas
+
+def get_user_auth(conn, user_id: int, role: str):
+    """
+    Fetches the user's password hash based on their role.
+    String formatting is safe here because `role` is strictly validated by Pydantic's Enum.
+    """
+    id_col = f"{role}_id"
+    query = f"SELECT {id_col} AS id, password_hash FROM {role} WHERE {id_col} = %s"
+    
+    with conn.cursor() as cur:
+        cur.execute(query, (user_id,))
+        return cur.fetchone()
+
+def create_student(conn, student: schemas.StudentCreate, hashed_password: str):
+    query = """
+        INSERT INTO student (student_id, first_name, last_name, email, phone, balance, password_hash, spending_limit)
+        VALUES (%s, %s, %s, %s, %s, %s, %s, %s)
+        RETURNING student_id;
+    """
+    with conn.cursor() as cur:
+        cur.execute(query, (
+            student.student_id, student.first_name, student.last_name,
+            student.email, student.phone, 0.0, hashed_password, student.spending_limit
+        ))
+        conn.commit()
+        return cur.fetchone()['student_id']
+
+def create_vendor(conn, vendor: schemas.VendorCreate, hashed_password: str):
+    query = """
+        INSERT INTO vendor (vendor_id, name, email, phone, password_hash)
+        VALUES (%s, %s, %s, %s, %s)
+        RETURNING vendor_id;
+    """
+    with conn.cursor() as cur:
+        cur.execute(query, (
+            vendor.vendor_id, vendor.name,
+            vendor.email, vendor.phone, hashed_password
+        ))
+        conn.commit()
+        return cur.fetchone()['vendor_id']
 
 def get_statement(conn, student_id: int):
     # raw sql query
